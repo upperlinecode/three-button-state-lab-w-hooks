3c9a551e68f224a20ffd829baa66686a3cc15599
Jeff <jolson615@gmail.com>
2022-04-11 17:47:31 -0400

    demo updated

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import "./App.css";
 import Nav from "./components/Nav.js";
 
 const App = () => {
-  // let user = "Wanda";
-  const [user, setUser] = useState("Wanda");
-
+  let user = "Wanda";
   const handleButton1 = () => {};
-
   const handleButton2 = () => {};
 
   return (
     <div className="App">
       <Nav />
       <div className="StateCard">
-        <h1>Welcome {user}</h1>
+        <h1>Welcome USER</h1>
         <div className="card-content">
           <div className="item">
             <button onClick={handleButton1}>xx</button>
